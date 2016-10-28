9b38035762fe59eb480988ecefa31eb2169b03bf
Dan Krijgsman <dan@properbo.co.uk>
2016-10-28 10:59:15 +0100

    added my name to readme

--- models/sausage.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const sausageSchema = new mongoose.Schema({
-  sausage: { type: String, required: true},
-  meat: String,
-  girth: Number
-});
-
-module.exports = mongoose.model("Sausage", sausageSchema);
