76c7fe216d4717e857a197a68e392146718c0459
Marco Servidio <servidiomarco@gmail.com>
2016-11-03 19:19:27 +0000

    last commit of the day

--- src/js/app.js
+++ src/js/app.js
@@ -140,18 +140,16 @@ $(() => {
           console.log(users[i].skillLevel);
           if(users[i].skillLevel === $skillLevel || $skillLevel === 'All Skill Levels') {
             $listUsers.append(`
-
-              <h4>${users[i].fullname}</h4>
-              <p><b>Location: </b>${users[i].postcode}</p>
-
-              <p><img src="${users[i].image}"class="userpic" alt="Image Coming"></p>
-
-              <b>Phone:</b><p>${users[i].phoneNumber}</p>
-              <p><b>Willing to travel</b>: ${users[i].travelDistance} miles</p>
-              <p><b>Typical availability</b>: ${users[i].availability}</p>
-              <p><b>Skill Level</b>: ${users[i].skillLevel}</p>
-              <a href="mailto:${users[i].email}"><button class="btn btn-info">Email</button></a>
-
+              <div class="user-card">
+                <h4>${users[i].fullname}</h4>
+                <p><img src="${users[i].image}"class="userImage" alt="Image Coming"></p>
+                <p><b>Location: </b>${users[i].postcode}</p>
+                <b>Phone:</b><p>${users[i].phoneNumber}</p>
+                <p><b>Willing to travel</b>: ${users[i].travelDistance} miles</p>
+                <p><b>Typical availability</b>: ${users[i].availability}</p>
+                <p><b>Skill Level</b>: ${users[i].skillLevel}</p>
+                <a href="mailto:${users[i].email}"><button class="btn btn-info">Email</button></a>
+              </div><br/>
               `);
 
               if (i==finish-1 && finish != users.length) {
